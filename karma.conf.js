fd637688e3674d4e39877be6d7fe46f8333ad539
Aniket Kulkarni <kaniket@mobileiron.com>
2020-02-10 11:51:40 +0530

    add docs

--- karma.conf.js
+++ karma.conf.js
@@ -7,8 +7,8 @@ module.exports = function(config) {
 
     frameworks: ['jasmine'],
 
-    // list of files / patterns to load in the browser
     files: ['src/index.tests.js'],
+
     webpack: webpackConfig,
     webpackMiddleware: {
       noInfo: true,
@@ -22,14 +22,8 @@ module.exports = function(config) {
       'src/**/*.test.js': ['webpack']
     },
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['spec', 'kjhtml'],
-
     port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
 
     // level of logging
@@ -52,8 +46,6 @@ module.exports = function(config) {
       }
     },
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
     singleRun: process.env.NODE_ENV !== 'dev',
 
     // Concurrency level
@@ -69,3 +61,5 @@ module.exports = function(config) {
     ]
   });
 };
+
+/* references: https://kamranicus.com/posts/2017-10-06-webpack-karma-jest-babel-angularjs */
