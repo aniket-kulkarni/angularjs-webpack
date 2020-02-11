589f0963d37dd6ba82d6938895e648189ef0452a
Aniket Kulkarni <kaniket@mobileiron.com>
2020-02-11 12:58:33 +0530

    remove unused comments

--- src/setup.test.js
+++ src/setup.test.js
@@ -1,8 +1,3 @@
 import 'angular';
 import 'angular-mocks';  //eslint-disable-line
 import './index';
-
-// require all test files using special Webpack feature
-// https://webpack.github.io/docs/context.html#require-context
-// const testsContext = require.context('.', true, /__tests__\/.*\.js$/);
-// testsContext.keys().forEach(testsContext);
